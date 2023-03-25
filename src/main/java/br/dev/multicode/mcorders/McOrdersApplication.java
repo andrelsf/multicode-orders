458778a644dc2b6d901f420a6bd9c51b9d0df9e5
Andre Ferreira <andre.dev.linux@gmail.com>
2023-03-25 06:19:35 -0300

    Initial commit

---
+++ src/main/java/br/dev/multicode/mcorders/McOrdersApplication.java
@@ -0,0 +1,13 @@
+package br.dev.multicode.mcorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McOrdersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(McOrdersApplication.class, args);
+  }
+
+}
